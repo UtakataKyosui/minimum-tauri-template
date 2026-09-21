1a27d4cc1e0ba6f3d8af856c96891e98e9eddcb6
UtakataKyosui <fill.ayaextech@gmail.com>
2026-09-21 22:58:54 +0900

    chore: TanStack Routerを入れる

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import {tanstackRouter} from '@tanstack/router-plugin/rspack';
 
 const host = process.env.TAURI_DEV_HOST;
 
@@ -17,6 +18,16 @@ export default defineConfig({
     } : undefined,
     watch: {
         ignored: ['**/node_modules/**', '**/.git/**',"**/src-tauri/**"]
+    }
+  },
+  tools: {
+    rspack: {
+        plugins: [
+            tanstackRouter({
+                target: "react",
+                autoCodeSplitting: true,
+            })
+        ]
     }
   }
 });
