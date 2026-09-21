97eb25346b5ff0c76c0694561be68b29df562815
UtakataKyosui <fill.ayaextech@gmail.com>
2026-09-21 22:49:45 +0900

    chore: ViteからRsbuildに変更

---
+++ rsbuild.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host ? {
+        protocol: 'ws',
+        host,
+        port: 1421,
+    } : undefined,
+    watch: {
+        ignored: ['**/node_modules/**', '**/.git/**',"**/src-tauri/**"]
+    }
+  }
+});
